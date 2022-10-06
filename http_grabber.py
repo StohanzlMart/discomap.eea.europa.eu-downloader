62d747c6d64d4303e658a747d6d8411bc26f93ff
Martin Stohanzl <31247214+StohanzlMart@users.noreply.github.com>
2022-10-06 18:42:01 +0200

    todo: sqlite saving and loading and NaNs

--- http_grabber.py
+++ http_grabber.py
@@ -1,12 +1,15 @@
 from functools import reduce
 from logging import basicConfig, debug, info, warn, error, DEBUG, INFO, WARN, ERROR
+import sqlite3
+from matplotlib import pyplot as plt
 import pandas as pd
 from requests import get
 from pathlib import Path
 from pprint import pprint, pformat
-from typing import List
+from typing import List, TypeVar
 from urllib.parse import parse_qs, parse_qsl, urlparse
 from collections import namedtuple
+from sqlalchemy import create_engine
 
 '''
 All functions neccesairy to get data from discomap.eea.eu:
@@ -82,6 +85,27 @@ def download_bulk_from_txt(file:Path, my_path:Path='csvs', my_encoding:str='utf-
     else:
         error(f'trying to open file that does not exist: {file}')
 
+def export_to_sqlite_db(df:pd.core.frame.DataFrame, folder:Path=''):
+    # (2) Enlesen in eine SQLITE DB
+    try:
+        # Erstellen einer SQLITE-DB im SQLITE-DB Verzeichnis
+        cnx = sqlite3.connect(Path(folder) / 'NO2.db')
+        # Schreiben der Daten
+        df.to_sql(name='NO2', con=cnx, if_exists="replace")
+        
+    except sqlite3.Error as e:
+        print("Error...", e)
+        
+    finally:
+        if (cnx):
+            cnx.close()
+            print("Connection Closed!")
+
+def import_from_sqlite_db():
+    engine = create_engine(Path() / 'NO2.db')
+    # (3) Auslesen der Daten aus der Datenbank
+    NO2 = pd.read_sql('select * from NO2', engine)
+
 def main():
     basicConfig(level=INFO)
     info('Started')
@@ -105,12 +129,38 @@ def main():
 
     #pprint(all_files)
 
-    fields = ['Concentration', 'DatetimeBegin', 'DatetimeEnd']
-    parse_dates = fields[1:2]
-    df_from_each_file = (pd.read_csv(f, sep=",", usecols=fields, parse_dates=parse_dates, encoding="utf-8") for f in all_files)
+    fields = ['DatetimeBegin', 'Concentration', 'DatetimeEnd']
+    parse_dates = fields[1:3]
+    print(parse_dates)
+    dtypes = {'DatetimeBegin': 'str', 'Concentration': 'float', 'DatetimeEnd': 'str'}
+    df_from_each_file = (pd.read_csv(f, sep=",", usecols=fields, dtype=dtypes, parse_dates=parse_dates, encoding="utf-8") for f in all_files)
     concatenated_df   = pd.concat(df_from_each_file, ignore_index=True)
-
+    #concatenated_df['diff'] = concatenated_df['DatetimeEnd'] - concatenated_df['DatetimeBegin']
+    concatenated_df = concatenated_df.drop('DatetimeEnd', axis=1)
     pprint(concatenated_df)
+    concatenated_df.to_csv('all.csv', sep=',', encoding='utf-8', index=False)
+
+    concatenated_df.set_index(['DatetimeBegin'],inplace=True)
+    my_df = concatenated_df.apply(pd.to_numeric, errors='coerce')
+    my_df.interpolate(method='linear', limit_direction='forward', axis=1)
+    my_df.info()
+    #concatenated_df.plot(concatenated_df["DatetimeBegin"], concatenated_df["Concentration"])
+    my_df.plot()
+    plt.show()
+
+    #fig, ax1 = plt.subplots()
+    #ax2 = ax1.twinx()
+    #h1, = ax1.plot(concatenated_df["DatetimeBegin"], concatenated_df["Concentration"], color='r', label='avg ppm')
+    #h2, = ax2.plot(co2["decimal"], co2["increase since 1800"], color='g', label='+ppm since 1800')
+    #plt.title('Average ppm CO2 in Atmosphere 1974 - 2022');
+    #plt.grid(True);
+    #plt.legend(handles=[h1, h2])
+    #plt.legend(handles=[h1])
+    #ax1.set_xlabel('Year')
+    #ax1.set_ylabel('CO2 [ppm]')
+    #ax2.set_ylabel('CO2 [ppm] increase since 1800')
+
+
 def main2():
     basicConfig(level=INFO)
     
